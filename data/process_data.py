07a0f2f213d03a4d64a6bbe9a96ef4d2c1cb74ee
cchen-sc <chenchenxi8@gmail.com>
2022-06-07 23:42:45 -0700

    Completed process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,48 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''Read in input csv files'''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''Split Categoties column into seperate category columns.
+    Then convert category values to just numbers 0 or 1.
+    Then concatenate new category matrix and remove duplicate rows.
+    '''
+    #create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';', expand = True)
+    #Use first row to get all categories and update column names
+    row = categories.iloc[0]
+    category_colnames = [x.split('-')[0] for x in row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    #Convert category valies to numbers: 0 and 1.
+    for column in categories:
+        categories[column] = categories[column].str.split('-').str[1]
+        categories[column] = pd.to_numeric(categories[column])
+    
+    # drop the original categories column from `df` and concatenate category matrix
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    #Remove duplicate rows
+    df.drop_duplicates(inplace=True)
+
+    return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+    '''Save the cleaned dataset into an sqlite database'''
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
